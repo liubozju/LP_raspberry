2837da1ef571c223ce0a2665dafccd5c447bb336
zhwang1517 <zh_w@zju.edu.cn>
2018-09-05 19:13:42 +0800

    delete all

--- doc/export.sdk.demo/mqtt.mk
+++
@@ -1,9 +0,0 @@
-all ::
-	$(CC) $(CFLAGS) -o ext.mqtt mqtt-example.c $(LDFLAGS)
-	$(CC) $(CFLAGS) -o ext.mqtt mqtt-example_tsl.c $(LDFLAGS)
-	$(CC) $(CFLAGS) -o ext.mqtt mqtt-example_robot.c $(LDFLAGS)
-	$(CC) $(CFLAGS) -o ext.mqtt mqtt-example_sensor.c $(LDFLAGS)
-
-clean ::
-	rm -vf ext.mqtt
-
